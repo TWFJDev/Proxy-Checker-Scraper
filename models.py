fb74a0539f77e840fb0a37eb6203acfa941ed5c7
Terrence Foulk <foulkterrence@gmail.com>
2025-12-18 05:40:43 -0600

    split scraper and checker

--- models.py
+++ models.py
@@ -1,30 +1,34 @@
 from sqlalchemy import create_engine
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Boolean
 from sqlalchemy.orm import declarative_base
 from uuid import uuid4
 
-engine = create_engine('sqlite:///proxies.sqlite', echo=False)
+engine = create_engine('sqlite:///proxies.db', echo=False)
 
 base = declarative_base()
+
+def gen_uuid():
+    return str(uuid4())
 
 class Proxies(base):
     __tablename__ = 'proxies'
 
-    id = Column(String(150), primary_key=True, default=str(uuid4()))
-    host = Column(String(150), nullable=False)
+    id = Column(String(150), primary_key=True, default=lambda: gen_uuid())
+    host = Column(String(150), nullable=True)
     port = Column(Integer)
     username = Column(String(150), nullable=True)
     password = Column(String(150), nullable=True)
-    site_checked_on = Column(String(150), nullable=False, default='N/A')
-    http = Column(String(150), nullable=False, default='Not Checked')
-    https = Column(String(150), nullable=False, default='Not Checked')
-    socks4 = Column(String(150), nullable=False, default='Not Checked')
-    socks5 = Column(String(150), nullable=False, default='Not Checked')
-    cloudflare = Column(String(150), nullable=False, default='N/A')
-    ping = Column(String(150), nullable=False, default='0 ms')
-    last_checked = Column(String(150), nullable=False, default='N/A')
+    site_checked_on = Column(String(150), nullable=True, default=None)
+    http = Column(Boolean, nullable=True, default=False)
+    https = Column(Boolean, nullable=True, default=False)
+    socks4 = Column(Boolean, nullable=True, default=False)
+    socks4a = Column(Boolean, nullable=True, default=False)
+    socks5 = Column(Boolean, nullable=True, default=False)
+    socks5h = Column(Boolean, nullable=True, default=False)
+    cloudflare = Column(Boolean, nullable=True, default=False)
+    last_checked = Column(String(150), nullable=True, default=None)
 
-    def __init__(self, host, port, username, password, site_checked_on, http, https, socks4, socks5, cloudflare, ping, last_checked):
+    def __init__(self, host, port, username, password, site_checked_on, http, https, socks4, socks4a, socks5, socks5h, cloudflare, last_checked):
         self.host = host
         self.port = port
         self.username = username
@@ -33,9 +37,10 @@ class Proxies(base):
         self.http = http
         self.https = https
         self.socks4 = socks4
+        self.socks4a = socks4a
         self.socks5 = socks5
+        self.socks5h = socks5h
         self.cloudflare = cloudflare
-        self.ping = ping
         self.last_checked = last_checked
 
 base.metadata.create_all(engine)
---
+++ scraper/scraper.py
@@ -0,0 +1,91 @@
+from sqlalchemy.orm import sessionmaker
+from contextlib import contextmanager
+import models, json, requests, re
+
+SessionLocal = sessionmaker(bind=models.engine)
+
+@contextmanager
+def get_session():
+    session = SessionLocal()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+def get_source_urls():
+    urls = []
+    with open('sources.json', 'r') as f:
+        lines = json.load(f)
+
+        for line in lines['sources']:
+            for protocol, url in line['protocols'].items():
+                if url:
+                    urls.append(url)
+
+    return urls
+
+def scrape():
+    urls = get_source_urls()
+    pattern = re.compile(r'://([\d\.]+:\d+)|^([\d\.]+:\d+)')
+
+    print(f'Scraping Proxies From {len(urls)} URLs!\n')
+
+    with get_session() as session:
+        existing_proxies = {(p.host, p.port) for p in session.query(models.Proxies.host, models.Proxies.port)}
+
+        i = 1
+
+        for url in urls:
+            with requests.Session() as req:
+                print(f'[{i}/{len(urls)}] | {url}')
+
+                i += 1
+
+                try:
+                    resp = req.get(url)
+                    resp.raise_for_status()
+                except requests.RequestException:
+                    print(f"Failed to fetch {url}")
+                    continue
+
+                for proxy in resp.text.split():
+                    line = proxy.strip().replace(' ', '_')
+                    match = pattern.search(line)
+                    if not match:
+                        continue
+
+                    proxy_match = match.group(1) or match.group(2)
+                    host, port = proxy_match.split(':')
+
+                    if (host, int(port)) in existing_proxies:
+                        continue
+
+                    new_proxy = models.Proxies(
+                        host=host,
+                        port=int(port),
+                        username=None,
+                        password=None,
+                        site_checked_on=None,
+                        http=None,
+                        https=None,
+                        socks4=None,
+                        socks4a=None,
+                        socks5=None,
+                        socks5h=None,
+                        cloudflare=None,
+                        last_checked=None
+                    )
+
+                    session.add(new_proxy)
+                    existing_proxies.add((host, int(port)))
+
+        print("\nInserting Proxies Into DB!")
+
+    print("\nFinished Scraping!")
+
+
+scrape()
